c2345b721efe267030f53ef6be75dfbaba63dcb5
Savino Jossi <3669673+sjossi@users.noreply.github.com>
2022-09-16 13:25:52 -0400

    refactor for setup.cfg

--- utils/pypi/setup.py
+++ utils/pypi/setup.py
@@ -1,27 +1,3 @@
 from setuptools import setup, find_packages
 
-
-setup(
-    name="uf2",
-    version="0.1.0",
-    url="https://github.com/microsoft/uf2",
-    description="Packing and unpacking UF2 files",
-
-    packages=find_packages(),
-
-    package_data={
-        "uf2": [
-            "uf2families.json"
-            ]
-        },
-
-    entry_points = {
-        "console_scripts": [
-            "uf2conv = uf2.uf2conv:main"
-            ]
-        }
-
-)
-
-if __name__ == "__main__":
-    setup()
+setup()
